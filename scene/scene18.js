dcff16177a0e898f56bd71178b19d482cb5547cc
yori <luzhixiang@hupu.com>
2015-07-09 11:31:40 +0800

    remove scene

--- scene/scene18.js
+++
@@ -1,9 +0,0 @@
-;require(['anole', 'zepto'], function(anole, Scene){
-    
-	var scene = new anole.Scene(18, anole.canvas, true);
-	// To be implemented
-	scene.createDom = function() {};
-    scene.animation = function () {};
-	scene.cleanup = function() {};
-	anole.addScene(scene);
-})
